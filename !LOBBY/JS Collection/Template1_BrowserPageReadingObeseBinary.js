c2a683bd43ce1ade334759155bf92160654555a0
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-07-01 17:21:02 -0700

    Homepage [5x] script [1x]
    The JavaScript lobby homepage, along with an archive of its first 4 versions. Those files are in HTML5 format. The other file, a JavaScript script is a template about browser reading.

---
+++ !LOBBY/JS Collection/Template1_BrowserPageReadingObeseBinary.js
@@ -0,0 +1,10 @@
+// [Browser name] [platform] x.x page reading section
+console.log("You won't receive this message: [Browser name] [platform] x.x is not compatible with this SNU page");
+function browsernameplatform_x_x_func {
+	console.log("Sniffing browser to check Browsername (Platform) version");
+	var sniff1 = 0;
+	if sniff1 = 1 {
+		console.log("This is not Browser name 0.8 (Platform)"):
+	} else {
+		console.log("Sniffing completed! Browser name 0.8 has been detected with [UA UNKNOWN]");
+}}
